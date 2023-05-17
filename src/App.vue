37ead009ac0765d3fd480e48a22eede8b1fc716b
dinosoupy <anishbasoup@gmail.com>
2023-05-17 20:07:11 +1000

    Modified App.vue to include sitewide navbar

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,33 @@
+<script>
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+      Navbar
+  }
+}
+</script>
+
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <Navbar></Navbar>
+  <div class="routerView">
+    <router-view/>
+  </div>
 </template>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  margin: 0px;
 }
-
-nav {
-  padding: 30px;
+.routerView {
+  overflow: hidden;
+  margin-top: 118px; /* Margin top is needed to offset the entire page content div below the navbar on page load. Navbar is a position: fixed type element so that content can go behind it when scrolling. This has the side effect of making normal content ignore navbar and hide behind it. For future reference, do not set a contant height property to navbar element - it will break the drop down animation. Always find out what height it happens to be and set margin-top here accordingly.*/
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+@media screen and (max-width:600px) {
+  .routerView {
+    margin-top: 80px; 
+  }
 }
 </style>
+
